f8ae17d69ad201abda59e3a8a8722d87fbb5a5b9
MichaelMoreno10 <116193017+MichaelMoreno10@users.noreply.github.com>
2023-03-29 19:49:45 -0500

    Delete ejercicio2.py

--- cadena/ejercicio2.py
+++
@@ -1,6 +0,0 @@
-# Solicite cadena e imprimala en todas las formas posibles en cuanto a mayusculas y minusculas
-nombre = "Michael Julian Moreno Diaz "
-nombre_lower =nombre.lower()
-print(nombre_lower)
-nombre_lower =nombre.upper()
-print(nombre_lower)
